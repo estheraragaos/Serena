65eb47fd4b512d039b2ab42d7ab50d5fe98522f3
Gabriele Andrade <gabbimdeandrade@gmail.com>
2020-08-19 13:21:52 -0300

    alterações finais

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 bot = ChatBot('Serena')
-bot = ChatBot(
-    'Serena',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri='sqlite:///database.sqlite3'
-    )
-    
+
 conversa = ListTrainer(bot)
 conversa.train([
-    'Oi!',
-    'Oi! Eu me chamo Serena, a assistente que vai mudar sua relação com o Serasa e deixar sua vida financeira do meu jeitinho: Serena!  E vc?',
-    'Eu me chamo',
-    'Prazer em te conhecer',
-    'Preciso de ajuda',
-    'Em que posso te ajudar?',
-    'Quero controlar meus gastos'
-
+    'Oi',
+    'Qual seu nome?',
+    'Gabriele',
+    'Oi! Eu me chamo Serena, a assistente que vai mudar sua relação com o Serasa e deixar sua vida financeira do meu jeitinho: Serena! Como posso começar a te ajudar?',
+    'Oi, quero consultar meu CPF',
+    'Claro! Digite o número do CPF sem pontos, vírgulas ou hífens, por favor!'
 ])
+
 while True:
     try:
         resposta = bot.get_response(input("Usuário: "))
